21eb2838848d97e60216f79ebc9ec3b207f45ec6
maria yudina <home82@list.ru>
2023-02-14 00:26:11 +0500

    Folder Example010_MethodArray was added

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {12,93,36,49,57,63,75,84,93,32};
+int n = array.Length;
+int find = 93;
+int index = 0;
+
+while(index<n)
+{
+  if (array[index]==find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
